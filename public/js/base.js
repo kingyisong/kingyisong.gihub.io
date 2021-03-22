ae156ff95baed66c024e7fea5a75aaad78930f37
jgfan <fanjinggang@cmos.chinamobile.com>
2021-03-22 17:55:42 +0800

    初步提交原型

--- public/js/base.js
+++ public/js/base.js
@@ -2,30 +2,34 @@
 function nav_click(is_show) {
   if (is_show) {
     /* 显示左侧aside */
-    $('.aside')
+    $('.navbar')
       .addClass('visible-md visible-lg')
       .removeClass('hidden-md hidden-lg')
-    /* 调整右侧内容 */
+    // /* 调整右侧内容 */
     $('.aside3')
-      .removeClass('col-md-13 col-lg-13')
-      .addClass('col-md-13 col-lg-13');
-    /* 调整文字内容格式 */
-    $('.aside3-content')
-      .removeClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2')
-      .addClass('col-md-13');
+        .removeClass('aside3-nonavbar')
+        .addClass('aside3-avbar')
+      // .addClass('col-md-13 col-lg-13');
+    // /* 调整文字内容格式 */
+    // $('.aside3-content')
+    //   .removeClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2')
+    //   .addClass('col-md-13');
   } else {
     /* 隐藏左侧aside */
-    $('.aside')
+    $('.navbar')
       .removeClass('visible-md visible-lg')
       .addClass('hidden-md hidden-lg');
-    /* 右侧内容最大化 */
     $('.aside3')
-      .removeClass('col-md-13 col-lg-13')
-      .addClass('col-md-13 col-lg-13');
-    /* 修改文字排版 */
-    $('.aside3-content')
-      .removeClass('col-md-13')
-      .addClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2'); 
+        .removeClass('aside3-navbar')
+        .addClass('aside3-nonavbar')
+    // /* 右侧内容最大化 */
+    // $('.aside3')
+    //   .removeClass('col-md-13 col-lg-13')
+    //   .addClass('col-md-13 col-lg-13');
+    // /* 修改文字排版 */
+    // $('.aside3-content')
+    //   .removeClass('col-md-13')
+    //   .addClass('col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2');
   }  /*col-md-offset-1 col-lg-offset-2*/
 }
 /* 控制文章章节列表按钮 */
